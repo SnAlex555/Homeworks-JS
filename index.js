0922f543ee9bcb514577dcccb1f9ce93ca5511f5
SnAlex <desert_eagle666@mail.ru>
2022-09-23 17:30:26 +0300

    edit Homework-1

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-const firstString = '# # # #';
-const secondString = ' # # # #';
+// const firstString = '# # # #';
+// const secondString = ' # # # #';
 
-let i
+// let i
 
-for (i = 0; i < 8; i++) {
-    if (i % 2 == 0) {
-        console.log(firstString)
+// for (i = 0; i < 8; i++) {
+//     if (i % 2 == 0) {
+//         console.log(firstString)
+//     } else {
+//         console.log(secondString)
+//     }
+// }
+
+let stringSize = 8;
+let chessBoard = "";
+
+for (let i = 0; i < stringSize; i++) {
+  for (let k = 0; k < stringSize; k++) {
+    if ((i + k) % 2 == 0) {
+        chessBoard += " ";
     } else {
-        console.log(secondString)
+        chessBoard += "#";
     }
+  }
+  chessBoard += "\n";
 }
+
+console.log(chessBoard);
